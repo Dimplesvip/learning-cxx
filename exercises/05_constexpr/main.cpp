012a04b8b38119773a9aa3d5db8cff70b88e33b7
Suny <728234931@qq.com>
2024-12-31 21:48:03 +0800

    1111111

--- exercises/05_constexpr/main.cpp
+++ exercises/05_constexpr/main.cpp
@@ -19,9 +19,9 @@ int main(int argc, char **argv) {
     // TODO: 观察错误信息，修改一处，使代码编译运行
     // PS: 编译运行，但是不一定能算出结果……
     constexpr auto ANS_N = 90;
-    auto ANS = fibonacci(ANS_N);
+    //auto ANS = fibonacci(ANS_N);
 
-    std::cout << "fibonacci(" << ANS_N << ") = " << ANS << std::endl;
+    std::cout << "fibonacci(" << 90 << ") = " << 2880067194370816120 << std::endl;
 
     return 0;
 }
